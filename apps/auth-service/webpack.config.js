bc15f5ff5032cd59e3e578c03a577aec61a9b10a
cbagajurel <shivaharigajurel@gmail.com>
2025-12-26 19:00:08 +0545

    build error fixes

---
+++ apps/auth-service/webpack.config.js
@@ -0,0 +1,32 @@
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join, resolve } = require('path');
+
+module.exports = {
+  output: {
+    path: join(__dirname, 'dist'),
+    clean: true,
+    ...(process.env.NODE_ENV !== 'production' && {
+      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    }),
+  },
+  resolve: {
+    alias: {
+      '@packages': resolve(__dirname, '../../packages'),
+      '@error-handler': resolve(__dirname, '../../packages/error-handler'),
+      '@lib': resolve(__dirname, '../../packages/lib'),
+    },
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: false,
+      sourceMap: true,
+    }),
+  ],
+};
